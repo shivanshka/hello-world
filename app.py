af3b91cd2b8d7619174af90f690b54f96b4b0beb
Shivansh Kaushal <kaushal.shivansh630@gmail.com>
2022-06-12 13:00:52 +0530

    message changed

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/",methods = ["GET","POST"])
 def index():
-    return "Machine Learning Application Started"
+    return "Machine Learning Application Started : Hello world"
 
 
 if __name__ == "__main__":
