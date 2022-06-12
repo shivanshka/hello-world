09e70f2d1884b94033b92654fe27304d90b244ea
Shivansh Kaushal <kaushal.shivansh630@gmail.com>
2022-06-12 21:10:40 +0530

    template and html added

--- app.py
+++ app.py
@@ -1,10 +1,10 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route("/",methods = ["GET","POST"])
 def index():
-    return "Machine Learning Application Started : Hello world ????"
+    return render_template("WineQuality.html")
 
 
 if __name__ == "__main__":
