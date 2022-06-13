bcbf03e64b189919c11e86a376285ce79de7cf8e
Shivansh Kaushal <kaushal.shivansh630@gmail.com>
2022-06-13 11:38:37 +0530

    commit

--- app.py
+++ app.py
@@ -4,11 +4,7 @@ app = Flask(__name__)
 
 @app.route("/",methods = ["GET","POST"])
 def index():
-<<<<<<< HEAD
     return render_template("WineQuality.html")
-=======
-    return "Machine Learning Application Started : Hello world"
->>>>>>> origin/main
 
 
 if __name__ == "__main__":
