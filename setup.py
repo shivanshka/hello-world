259c37c4ea7b93686d68ef877f0f21f7ffd5fec0
Shivansh Kaushal <kaushal.shivansh630@gmail.com>
2022-06-18 13:20:12 +0530

    exception and logger file added

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ PROJECT_NAME="housing-predictor"
 VERSION="0.0.2ins"
 AUTHOR="Shivansh Kaushal"
 DESCRIPTION="This is my first end to end project"
-PACKAGES=["housing"]
+#PACKAGES=["housing"]
 REQUIREMENT_FILE_NAME="requirements.txt"
 
 def get_requirements_list()->List[str]:
@@ -14,14 +14,14 @@ def get_requirements_list()->List[str]:
     Description: This function is going to return list of requirement mentioned in requirements.txt
     """
     with open(REQUIREMENT_FILE_NAME) as requirement_file:
-        return requirement_file.readlines()  #.remove("-e .")
+        return requirement_file.readlines().remove("-e .")
 
 setup(
     name = PROJECT_NAME,
     version=VERSION,
     author=AUTHOR,
     description=DESCRIPTION,
-    packages=PACKAGES,    # find_packages()
+    packages=find_packages(),
     install_requires=get_requirements_list()
 )
 
