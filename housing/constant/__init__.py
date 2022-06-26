2d10b9984b653c4b84ed2f4cdb2f3f5f636af4c5
Shivansh Kaushal <kaushal.shivansh630@gmail.com>
2022-06-26 13:26:15 +0530

    data igestion done

--- housing/constant/__init__.py
+++ housing/constant/__init__.py
@@ -10,12 +10,12 @@ CONFIG_FILE_PATH = os.path.join(ROOT_DIR,CONFIG_DIR,CONFIG_FILE_NAME)
 CURRENT_TIME_STAMP = f"{datetime.now()}.strftime('%Y-%m-%d-%H-%M-%S')"
 
 
-# Training pipeline related variable
+# Training pipeline related variables
 TRAINING_PIPELINE_CONFIG_KEY = "training_pipeline_config"
 TRAINING_PIPELINE_ARTIFACT_DIR_KEY = "artifact_dir"
 TRAINING_PIPELINE_NAME_KEY = "pipeline_name"
 
-# Data Ingestion related variable
+# Data Ingestion related variables
 
 DATA_INGESTION_CONFIG_KEY = "data_ingestion_config"
 DATA_INGESTION_ARTIFACT_DIR = "data_ingestion"
@@ -25,3 +25,27 @@ DATA_INGESTION_TGZ_DOWNLOAD_DIR_KEY = "tgz_download_dir"
 DATA_INGESTION_INGESTED_DIR_NAME_KEY = "ingested_dir"
 DATA_INGESTION_TRAIN_DIR_KEY = "ingested_train_dir"
 DATA_INGESTION_TEST_DIR_KEY = "ingested_test_dir"
+
+# Data Validation related variables
+
+DATA_VALIDATION_CONFIG_KEY = "data_validation_config"
+DATA_VALIDATION_SCHEMA_FILE_NAME_KEY = "schema_file_name"
+
+# Data Transfromation related variables
+
+DATA_TRANSFORMATION_CONFIG_KEY = "data_transformation_config"
+
+# Model Training related variables
+
+MODEL_TRAINER_CONFIG_KEY = "model_trainer_config"
+
+# Model Evaluation related variables
+
+MODEL_EVALUATION_CONFIG_KEY = "model_evaluation_config"
+MODEL_EVALUATION_FILE_NAME_KEY = "model_evaluation_file_name"
+
+# Model Evaluation related variables
+
+MODEL_PUSHER_CONFIG_KEY = "model_pusher_config"
+MODEL_EXPORT_DIR_KEY = "model_export_dir"
+
--- housing/exception/__init__.py
+++ housing/exception/__init__.py
@@ -16,7 +16,7 @@ class HousingException(Exception):
 
         _, _, exec_tb = error_details.exc_info()
 
-        line_number = exec_tb.tb_frame.f_lineno
+        line_number = exec_tb.tb_lineno
         file_name = exec_tb.tb_frame.f_code.co_filename
 
         error_message = f"Error occured in script: [{file_name}] at line number: [{line_number}] error message: [{error_message}]"
